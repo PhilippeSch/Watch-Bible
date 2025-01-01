c9b66bda1fbdb717604bd5ae1c92733d828f74e6
Philippe Scheuber <philippe@scheuber.me>
2025-01-01 21:00:16 +0100

    Initial Commit

---
+++ Watch Bible Watch App/Watch_BibleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Watch_BibleApp.swift
+//  Watch Bible Watch App
+//
+//  Created by Philippe Scheuber on 01.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct Watch_Bible_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Watch Bible Watch AppTests/Watch_Bible_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Watch_Bible_Watch_AppTests.swift
+//  Watch Bible Watch AppTests
+//
+//  Created by Philippe Scheuber on 01.01.2025.
+//
+
+import Testing
+@testable import Watch_Bible_Watch_App
+
+struct Watch_Bible_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
